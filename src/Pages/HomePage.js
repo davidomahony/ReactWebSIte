66b2febafddde7b4139146275a161a41a7a71944
David-R OMahony <David-R.OMahony@analog.com>
2020-04-25 12:20:38 +0100

    Changes: Added AddressForm.js Added Checkout.js Added Footer.js
    Started Looking @ HomeScreen

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,21 +1,56 @@
 import React from 'react'
-import {Card, Navbar} from 'react-bootstrap'
+import {Card, Navbar, Pagination, PageItem, Carousel, Button} from 'react-bootstrap'
+
+import ReactPlayer from 'react-player'
+
+import './HomePage.scss'
+
+import ReviewImage from './../Photos/ReviewImages.jpg'
+
+import Footer from  './../Components/Footer'
 
 class HomePage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        reviewSlides: ['Slide One', 'Slide Two', 'Slide Three', 'Slide Four'],
+        numberofSlide : Math.floor(window.innerWidth/400)
+    }
+  }
+
+    GetReviewSlides(){
+      let displayReviewSlides = this.state.reviewSlides.map(slide => 
+        <Carousel.Item>
+          <div>
+          <img src={ReviewImage}></img>
+          </div>
+          <Carousel.Caption>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus iaculis mollis ligula sed ultrices.
+          </Carousel.Caption>
+        </Carousel.Item>
+        )
+      return displayReviewSlides
+  }
+
   render() {
     return (
-      <div>
-        <Card classname="pageCard">
-          <Card.Header>
-            Home Need to do
+      <div classname="pageCard">
+            <Card.Header>
+              <div className="centerHorizontal">
+                <h2>
+                 Home
+                </h2>
+              </div>
           </Card.Header>
-          <Card.Body>
-
-          </Card.Body>
-          <Card.Footer>
-            Footer prob have checkout
-          </Card.Footer>
-        </Card>
+          <ReactPlayer classname="player"
+            width='100%'
+            height='100%'
+            url='https://www.youtube.com/watch?v=o_RXZyojl8k&list=RDo_RXZyojl8k&start_radio=1'/>
+          <Carousel>
+            {this.GetReviewSlides()}
+          </Carousel>
+          <Footer WhatAction="GoToSelectPhoto" IsButtonEnabled={true}/>
       </div>
     )
   }
--- src/Pages/SelectPhoto.js
+++ src/Pages/SelectPhoto.js
@@ -9,9 +9,8 @@ import StyleThree from "./../Photos/everIcon.svg";
 
 import UploadButton from './../Components/UploadButton'
 import Header from './../Components/Header'
-
-import SlidingPane from 'react-sliding-pane';
-import 'react-sliding-pane/dist/react-sliding-pane.css';
+import Footer from  './../Components/Footer'
+import Checkout from '../Components/Checkout';
 
 class SelectPhoto extends React.Component {
   constructor(props) {
@@ -54,10 +53,6 @@ photoAdded = (photosrc, croppedsrc, name) => {
   this.setState({uploadedPhotos: currentPhotos})
 }
 
-closeCropper = () => {
-  this.setState({showCropperModal: false})
-}
-
 removePhoto = (photo) => {
   let newPhotos = this.state.uploadedPhotos.filter(p => p.name !== photo.name)
   this.setState({uploadedPhotos: newPhotos})
@@ -90,18 +85,6 @@ GetAvailableStyles(){
     )
 }
 
-checkOutExit(){
-  return(
-    <div>
-      <i class="fa fa-window-close fa-3x red" aria-hidden="true"></i>
-    </div>
-  )
-  }
-
-  ShowCheckout = () =>{
-    this.setState({showCheckout: true})
-  }
-
   render() {
     return (
       <div className="pageCard">
@@ -118,55 +101,18 @@ checkOutExit(){
           </div>
           <div className="scrollmenuPreview">
             <div className="vcentre">
-              <UploadButton closeCropper={this.closeCropper} 
+              <UploadButton closeCropper={() => this.setState({showCropperModal: false})} 
                 removePhoto={this.removePhoto}
                 showModal={this.state.showCropperModal} 
                 imageForCrop={this.state.imageForCrop} photoAdded={this.photoAdded}/>
             </div>           
             {this.GetAvailablePreviews(this.state.uploadedPhotos)}
           </div>
-          <SlidingPane
-                isOpen={ this.state.showCheckout }
-                title='Checkout'
-                from='right'
-                width='400px'
-                onRequestClose={() => this.setState({ showCheckout: false }) }>
-                <div>
-                <hr/>
-                  <Container>
-                    <Row>
-                      <i class="fa fa-home fa-2x" aria-hidden="true"></i>
-                      <h4>   Address </h4>
-                    </Row>
-                    <Row>
-                      <h4>   form to get address</h4>
-                    </Row>
-                    <hr/>
-                    <Row>
-                      <h5> Summary </h5>
-                    </Row>
-                    <Row>
-                      <h7>
-                        Number of Images : {this.state.uploadedPhotos.length}
-                      </h7>
-                    </Row>
-                    <Row>
-                    <h7>
-                       Style Selected: {this.state.activeStyle.name}
-                      </h7>
-                    </Row>
-                    <hr/>
-                  </Container>
-                 Check this out bitch
-                </div>
-            </SlidingPane>
-            <div className="footer">
-            <Card.Footer>
-              <Button onClick={this.ShowCheckout} className="footerButton">
-              Checkout
-            </Button>
-            </Card.Footer>
-          </div>
+          <Checkout showCheckout={this.state.showCheckout} 
+            closeCheckout={() => this.setState({showCheckout : false})}
+            activeStyle={this.state.activeStyle}
+            uploadedPhotos={this.state.uploadedPhotos} />
+          <Footer WhatAction="GoToCheckOut" Action={() => this.setState({showCheckout: true})} IsButtonEnabled={true}/>
       </div>
     )
   }
