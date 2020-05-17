6daf39dcd96e38468949d72d8bdf2bb984de28cb
JD <djjdomahony@gmail.com>
2020-05-17 14:14:26 +0100

    no message

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -54,7 +54,7 @@ class HomePage extends React.Component {
           </Card.Header>
           <div className="headerArea bg-light">
           <div className="headerText">SticPics are beautiful real wood products that attach easily to walls.</div> 
-          <div className="headerText2">Love Real Wood. Love Sustainability</div>  
+         
           <div className="promoVideo">
                
                <img className = "promoVideo"src={promoVideo}></img>
@@ -73,9 +73,12 @@ One Perfect Size - Removable, reusable and leave no marks</div>
   <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.283.95l-3.523 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
 </svg><svg class="bi bi-star-fill text-primary" width="2em" height="2em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
   <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.283.95l-3.523 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
-</svg></div> 
+</svg>
+<p>Shipping is always free!</p>
+</div> 
 <div className="priceText bg-light">
 3 for €39.99 - Only €10 per extra tile!</div>
+<div className="headerText2">Love Real Wood. Love Sustainability</div> <i class="fas fa-boxes"></i> 
             <Slider dots={true} slidesToShow={this.state.numberofSlides} >
             {this.GetReviewSlides()}
             </Slider>
